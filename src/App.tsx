48d3998699d4b7ee9ff683c73b2049001acf3207
thecodingfreake <114638726+thecodingfreake@users.noreply.github.com>
2024-02-26 08:09:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import HomePage from './Pages/HomePage'
+import EditForm from './Components/EditForm';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 function App() {
-
   return (
-    <>
-    <HomePage/>
-    </>
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/add" element={<EditForm />} />
+    </Routes>
+  </Router>
   )
 }
+
 
 export default App
